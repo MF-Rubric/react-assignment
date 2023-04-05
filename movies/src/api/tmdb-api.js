286f96201b1408e6550da70dd2f76ba3cfa8c51c
Mark <20025214@wit.ie>
2023-04-05 15:45:21 +0100

    review

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -91,7 +91,7 @@ export const getMovie = (args) => {
 
   export const getShows = () => {
     return fetch(
-      ` https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+      ` https://api.themoviedb.org/3/discover/tv?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&sort_by=popularity.desc&page=1&timezone=Europe%2FDublin&include_null_first_air_dates=false&with_watch_monetization_types=flatrate&with_status=0&with_type=0`
     ).then((response) => {
       if (!response.ok) {
         throw new Error(response.json().message);
